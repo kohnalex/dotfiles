02b899bb6eb734372ece70a5cf8150fe25e3cf1e
Alexander Köhn <kohnalex2@gmail.com>
2021-10-23 14:29:04 +0200

    fish - add icons to tree view

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,7 +23,7 @@ if type -q exa
 	alias ls='exa -al --color=always --group-directories-first' # my preferred listing
 	alias la='exa -a --color=always --group-directories-first'  # all files and dirs
 	alias ll='exa -l --color=always --group-directories-first'  # long format
-	alias lt='exa -aT --color=always --group-directories-first' # tree listing
+	alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
 	alias l.='exa -a | egrep "^\."'
 	alias l='ls'
 end
