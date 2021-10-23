5063ee00d078436af60d702a7f6dc28bc0a4420a
Alexander Köhn <kohnalex2@gmail.com>
2021-10-23 17:24:19 +0200

    fish - cleaner config
    - fzf function are wip

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,75 +1,109 @@
+#    __    _  _ 
+#   /__\  ( )/ )
+#  /(__)\  )  ( 
+# (__)(__)(_)\_)
+#
+# Alexander Köhn, 2021
+                
+                
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	# set homebrew path environment
+	eval (/opt/homebrew/bin/brew shellenv)
 end
 
-### STANDARD STUFF ###
+###
+### STANDARD STUFF
+###
 set fish_greeting
 
-### ENV VARIABLES ###
-# eval "(/opt/homebrew/bin/brew shellenv)"
-# export PATH=~/Library/Jetbrains/bin:$PATH
-# export PATH="/opt/homebrew/opt/node@14/bin:$PATH"
-if status --is-interactive
-  eval (/opt/homebrew/bin/brew shellenv)
-end
+###
+### ADD PATH
+###
 fish_add_path ~/Library/Jetbrains/bin
 fish_add_path /opt/homebrew/opt/node@14/bin
 
+###
+### ALIASES & CUSTOM FUNCTIONS
+###
 
-### ALIASES ###
-
-# exa statt ls
+### EXA
 if type -q exa
-	alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+	alias ls='exa -l --color=always --group-directories-first' # my preferred listing
 	alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-	alias ll='exa -l --color=always --group-directories-first'  # long format
-	alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+	alias ll='exa -al --color=always --group-directories-first'  # long format
+	alias lt='exa -aT --color=always --group-directories-first' # tree listing
 	alias l.='exa -a | egrep "^\."'
 	alias l='ls'
 end
 
+### BAT
+if type -q bat
+	alias cat='bat'
+end
 
-# Brew vs. MacOS Remappings
-alias gcc='gcc-11'
-alias cc='gcc-11'
-alias g++='g++-11'
-alias c++='c++-11'
-alias vim="/opt/homebrew/bin/vim"
 
-# Sonstige
-alias ff='cd_with_fzf_home'
-alias ffc='cd_with_fzf'
-alias tt='tree -a -C -L 2'
+### GCC
+if type -q gcc-11
+	alias gcc='gcc-11'
+	alias cc='gcc-11'
+	alias g++='g++-11'
+	alias c++='c++-11'
+	alias vim="/opt/homebrew/bin/vim"
+end
+
+### FZF
+if type -q fzf
+	alias ff='fd . --hidden | fzf --preview "bat {-1} --color=always"'
+	alias ffcat='ff | xargs bat'
+
+	# Search for file and open with vim
+	function ffvim
+		set tmpvar (ff)
+		if test -n "$tmpvar"
+			vim $tmpvar
+		end
+		
+		# clear variables
+		set -e tmpvar
+	end
+
+
+	function ffdiff
+		set preview "git diff $argv --color=always -- {-1}"
+		# if type -q bat 
+		# 	set preview "bat"
+		# end
+		git diff $argv --name-only --relative | fzf -m --ansi --preview $preview
+
+		set -e preview
+	end
+end
+
+### MISC
 alias cdc="cd ~/Documents/Code"
 alias cdd="cd ~/.dotfiles"
 alias cdf="cd ~/.dotfiles/fish/.config/fish"
 alias md="mkdir"
-alias py="python3"
-alias t="tmux"
-alias ta="tmux attach"
-
-# alias khnlx="ssh -i ~/.ssh/my_keys/khnlx_id_rsa -t alex@h2888247.stratoserver.net"
 alias khnlx="ssh khnlx@h2888247.stratoserver.net"
+alias sfish="source ~/.config/fish/config.fish"
 
-# docker
-alias dockervm='docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i sh'
-alias dockerbash='docker_exec_bash'
-function docker_exec_bash
-	docker exec -it "$1" /bin/bash
-end
-
-# Eigene Funktionen
-# Ordner erstellen und reingehen
+# Create directory and cd into
 function mdc
     mkdir -p -- "$1" &&
     cd -P -- "$1"
 end
 
-# Ordner mit fzf finden mit $HOME als root
-function cd_with_fzf_home 
-    cd $HOME && cd "(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview")" && pwd && tree -a -C -L 1
+### TMUX
+alias t="tmux"
+alias ta="tmux attach"
+
+### DOCKER
+alias dockervm='docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i sh'
+alias dockerbash='docker_exec_bash'
+
+# Execute bash(!) shell on given container
+function docker_exec_bash
+	docker exec -it "$1" /bin/bash
 end
-# Ordner mit fzf finden mit (pwd) als root
-function cd_with_fzf 
-    cd "(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview")" && pwd && tree -a -C -L 1
-end 
+
