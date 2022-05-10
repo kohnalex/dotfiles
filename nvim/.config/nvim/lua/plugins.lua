c00afe8f2ea7af0649d458e394d73e68ddcd2ece
Alexander Köhn <kohnalex2@gmail.com>
2022-05-10 19:37:19 +0200

    -added: nvim: plugin descriptions

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,12 +1,27 @@
 return require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'vim-airline/vim-airline'
+    -- packer manages itseld
+    use 'wbthomason/packer.nvim' 
+
+    -- status line
+    use 'vim-airline/vim-airline' 
     use 'vim-airline/vim-airline-themes'
+
+    -- colorschemes
     use 'morhetz/gruvbox'
+
+    -- auto closing brackets, spaces, etc.
     use 'cohama/lexima.vim'
+
+    -- git status annotation in buffer
     use 'mhinz/vim-signify'
+
+    -- op git pplugin
     use 'tpope/vim-fugitive'
+
+    -- auto commenting with 'gcc'
     use 'tpope/vim-commentary'
+
+    -- better highlighting and parsing
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ":TSUpdate"
