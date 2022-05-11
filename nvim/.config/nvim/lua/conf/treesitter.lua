2ea30938768746cfee06ec43a5960fc141ef63f6
Alexander Köhn <kohnalex2@gmail.com>
2022-05-11 11:54:00 +0200

    -fixed: format some lua files

--- nvim/.config/nvim/lua/conf/treesitter.lua
+++ nvim/.config/nvim/lua/conf/treesitter.lua
@@ -1,37 +1,37 @@
 -- require treesitter safely
 local ok, configs = pcall(require, 'nvim-treesitter.configs')
 if not ok then
-    print 'not ok'
+  print 'not ok'
 end
 
 configs.setup {
-    ensure_installed = {
-        "typescript",
-        "javascript",
-        "vue",
-        "java",
-        "python",
-        "yaml",
-        "lua",
-        "fish",
-        "bash",
-        "tsx",
-        "css",
-        "scss",
-        "html",
-        "dockerfile",
-        "c",
-        "cpp",
-    },
-    highlight = {
-        enable = true,
-    },
-    indent = {
-        enable = true,
-        disable = {
-            "yaml",
-        }
+  ensure_installed = {
+    "typescript",
+    "javascript",
+    "vue",
+    "java",
+    "python",
+    "yaml",
+    "lua",
+    "fish",
+    "bash",
+    "tsx",
+    "css",
+    "scss",
+    "html",
+    "dockerfile",
+    "c",
+    "cpp",
+  },
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+    disable = {
+      "yaml",
     }
+  }
 }
 
 vim.opt.foldmethod = "expr"
