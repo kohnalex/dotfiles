5a2c1745bce7d67dc0ad7a0034e793a2d8060979
Alexander Köhn <alexander.koehn@ahearo.com>
2022-05-23 15:45:55 +0200

    changed: move split pane size by 2 instead of 1

--- nvim/.config/nvim/after/plugin/keymaps.lua
+++ nvim/.config/nvim/after/plugin/keymaps.lua
@@ -35,7 +35,7 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", default_opts)
 
 -- Resizing panes
 -- Description: Use arrow keys to resize the split window. Currently conflicting with mouse scrolling.
-keymap("n", "<S-Right>", ":vertical resize +1<CR>", default_opts)
-keymap("n", "<S-Left>", ":vertical resize -1<CR>", default_opts)
-keymap("n", "<S-Up>", ":resize -1<CR>", default_opts)
-keymap("n", "<S-Down>", ":resize +1<CR>", default_opts)
+keymap("n", "<S-Right>", ":vertical resize +2<CR>", default_opts)
+keymap("n", "<S-Left>", ":vertical resize -2<CR>", default_opts)
+keymap("n", "<S-Up>", ":resize -2<CR>", default_opts)
+keymap("n", "<S-Down>", ":resize +2<CR>", default_opts)
