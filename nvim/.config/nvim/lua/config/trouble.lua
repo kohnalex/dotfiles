fcaf4ab8d2d21b8cd75c5d37b4279bad15245a12
Alexander Köhn <kohnalex2@gmail.com>
2022-06-11 12:02:35 +0200

    changed: even less nvim nf

--- nvim/.config/nvim/lua/config/trouble.lua
+++ nvim/.config/nvim/lua/config/trouble.lua
@@ -10,8 +10,10 @@ function M.setup()
     width = 50, -- width of the list when position is left or right
     icons = true, -- use devicons for filenames
     mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-    fold_open = "", -- icon used for open folds
-    fold_closed = "", -- icon used for closed folds
+    fold_open = "-", -- icon used for open folds
+    fold_closed = "+", -- icon used for closed folds
+    -- fold_open = "", -- icon used for open folds
+    -- fold_closed = "", -- icon used for closed folds
     group = true, -- group results by file
     padding = true, -- add an extra new line on top of the list
     action_keys = { -- key mappings for actions in the trouble list
