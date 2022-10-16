769fec629b2e6762b60b6f6dce1367f812ca85c9
Alexander Köhn <kohnalex2@gmail.com>
2022-10-16 17:48:12 +0200

    added: colortheme

---
+++ nvim/.config/nvim/lua/user/colors.lua
@@ -0,0 +1,5 @@
+-- TODO: Very weird coloring going on with catppuccin recently..
+
+vim.opt.bg = "dark"
+vim.g.catppuccin_flavour = "macchiato"
+vim.api.nvim_command("colorscheme catppuccin")
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -4,4 +4,5 @@ if not packer_ok then return end
 packer.startup(function(use)
     use 'wbthomason/packer.nvim' -- Packer manages itself
     use 'nvim-treesitter/nvim-treesitter' -- Better highlight
+    use 'catppuccin/nvim' -- Colortheme
 end)
