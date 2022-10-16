25de536bcb2d6b956e9c6bbd3ae7288e765a32ed
Alexander Köhn <kohnalex2@gmail.com>
2022-10-16 18:27:09 +0200

    added: cmp

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -5,4 +5,13 @@ packer.startup(function(use)
     use 'wbthomason/packer.nvim' -- Packer manages itself
     use 'nvim-treesitter/nvim-treesitter' -- Better highlight
     use 'catppuccin/nvim' -- Colortheme
+
+    -- Code completion
+    use 'saadparwaiz1/cmp_luasnip' -- Snippet Engine. Required by cmp to run
+    use 'hrsh7th/nvim-cmp' -- Main plugin
+    use 'hrsh7th/cmp-buffer' -- Buffer Completions
+    use 'hrsh7th/cmp-path' -- Path Completions
+    use 'hrsh7th/cmp-cmdline' -- Cmdline Completions
+    use 'hrsh7th/cmp-nvim-lsp' -- LSP Completions
+    use 'rafamadriz/friendly-snippets' -- A collection of fine snippets
 end)
