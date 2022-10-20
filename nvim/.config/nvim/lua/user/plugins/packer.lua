05d4ea8d6eab13ff5bb2054890887fe6c3868124
Alexander Köhn <kohnalex2@gmail.com>
2022-10-20 05:22:24 +0200

    added: autopairs

--- nvim/.config/nvim/lua/user/plugins/packer.lua
+++ nvim/.config/nvim/lua/user/plugins/packer.lua
@@ -15,6 +15,8 @@ packer.startup(function(use)
     use 'tpope/vim-sleuth' -- Smart tabs & spaces
     use 'tpope/vim-commentary' -- Quick code commenting
     use 'tpope/vim-surround' -- Quick quote editing
+    use 'windwp/nvim-autopairs' -- autoclose parens, brackets, quotes, etc...
+    use 'windwp/nvim-ts-autotag' -- autoclose tags
     use 'christoomey/vim-tmux-navigator' -- Seemless Vim and Tmux navigator
 
     -- LSP
