a2fd506b430fe0a2ef87f3aea4cded7ab39dac48
Alexander Köhn <kohnalex2@gmail.com>
2022-10-27 10:55:37 +0200

    added: basic keymaps

--- nvim/.config/nvim/lua/user/plugins/keymaps.lua
+++ nvim/.config/nvim/lua/user/plugins/keymaps.lua
@@ -8,7 +8,27 @@ local default_opts = { noremap = true, silent = true }
 
 -- Nvimtree
 function M.setup_nvimtree_keymap()
-	keymap("n", "<C-n>", ":NvimTreeToggle<CR>", default_opts)
+  keymap("n", "<C-n>", ":NvimTreeToggle<CR>", default_opts)
+end
+
+-- LSP
+function M.setup_lsp_keymap(client, bufnr)
+  -- keybind options
+  local opts = { noremap = true, silent = true}
+
+  -- set keybinds
+  keymap("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+  keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+  keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+  keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+  keymap("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+  keymap("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+  keymap("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+  keymap("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+  keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+  keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+  keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+  keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
 end
 
 return M
--- nvim/.config/nvim/lua/user/plugins/lsp/mason_lsp.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/mason_lsp.lua
@@ -4,30 +4,58 @@ if not mason_lsp_ok then return end
 local lspconfig_ok, lspconfig = pcall(require, 'lspconfig')
 if not lspconfig_ok then return end
 
-local servers = {
-    'tsserver',
-    'sumneko_lua',
-}
+local cmp_nvim_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if not cmp_nvim_ok then return end
 
 mason_lsp.setup({
-    ensure_installed = servers
+    ensure_installed = {
+    'tsserver',
+    'sumneko_lua',
+    'pyright',
+    }
 })
 
 local on_attach = function(client, bufnr)
+		require("user.plugins.keymaps").setup_lsp_keymap(client, bufnr);
     -- TODO: Setup on attach
 end
+
+-- used to enable autocompletion (assign to every lsp server config)
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
 local lsp_flags = {
     debounce_text_changes = 150 -- default
 }
 
+lspconfig['pyright'].setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    flags = lsp_flags
+}
+
 lspconfig['tsserver'].setup {
+    capabilities = capabilities,
     on_attach = on_attach,
     flags = lsp_flags
 }
 
 
 lspconfig['sumneko_lua'].setup {
-    on_attach = on_attach,
-    flags = lsp_flags
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = { -- custom settings for lua
+		Lua = {
+			-- make the language server recognize "vim" global
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				-- make language server aware of runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.stdpath("config") .. "/lua"] = true,
+				},
+			},
+		},
+	},
 }
