7a72ea21fb6c81838440a376c9360854debdeda5
Alexander Köhn <kohnalex2@gmail.com>
2022-10-30 14:46:48 +0100

    changed: cleaner nls config

--- nvim/.config/nvim/lua/user/plugins/lsp/null_ls.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/null_ls.lua
@@ -7,30 +7,34 @@ local code_actions = nls.builtins.code_actions
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local sources = {
+  formatting.prettierd,
+  formatting.stylua,
+  diagnostics.eslint_d,
+  code_actions.gitsigns,
+}
+
+local on_attach = function(current_client, bufnr)
+  if current_client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({
+          filter = function(client)
+            --  only use null-ls for formatting instead of lsp server
+            return client.name == "null-ls"
+          end,
+          bufnr = bufnr,
+        })
+      end,
+    })
+  end
+end
+
 nls.setup({
-  sources = {
-    formatting.prettierd,
-    formatting.stylua,
-    diagnostics.eslint_d,
-    code_actions.gitsigns,
-  },
-  -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
-        end,
-      })
-    end
-  end,
+  sources = sources,
+  on_attach = on_attach,
+  debounce = 150,
 })
