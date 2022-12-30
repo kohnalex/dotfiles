9ad6675337d638960f440d4338fd1087b5c0514c
Alexander Köhn <kohnalex2@gmail.com>
2022-12-30 21:21:48 +0100

    fix: cmp breaks sometimes tab

--- nvim/.config/nvim/lua/user/plugins/cmp.lua
+++ nvim/.config/nvim/lua/user/plugins/cmp.lua
@@ -41,11 +41,11 @@ local kind_icons = {
   -- more here: https://www.nerdfonts.com/cheat-sheet
 }
 
--- Better backspace for supertab
-local check_backspace = function()
-  local col = vim.fn.col(".") - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
+-- -- Better backspace for supertab
+-- local check_backspace = function()
+--   local col = vim.fn.col(".") - 1
+--   return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+-- end
 
 -- Setup
 ---@diagnostic disable-next-line: redundant-parameter
@@ -68,32 +68,32 @@ cmp.setup({
     }),
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    -- ["<Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_next_item()
+    --   elseif luasnip.expandable() then
+    --     luasnip.expand()
+    --   elseif luasnip.expand_or_jumpable() then
+    --     luasnip.expand_or_jump()
+    --   elseif check_backspace() then
+    --     fallback()
+    --   else
+    --     fallback()
+    --   end
+    -- end, { "i", "s" }),
+    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_prev_item()
+    --   elseif luasnip.jumpable(-1) then
+    --     luasnip.jump(-1)
+    --   else
+    --     fallback()
+    --   end
+    -- end, {
+    --   "i",
+    --   "s",
+    -- }),
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
