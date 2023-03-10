988108b341e979ad6ac3bed459cbfe917e343aeb
Alexander Köhn <kohnalex2@gmail.com>
2023-03-10 14:01:06 +0100

    added: build tag for gopls

--- nvim/.config/nvim/lua/user/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/lspconfig.lua
@@ -43,6 +43,11 @@ lspconfig["pyright"].setup({
 lspconfig["gopls"].setup({
   capabilities = capabilities,
   on_attach = on_attach,
+  settings = {
+    gopls = {
+      buildFlags = { "-tags=wireinject" },
+    },
+  },
 })
 
 lspconfig["rust_analyzer"].setup({
