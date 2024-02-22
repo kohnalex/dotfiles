ebeaf4f635ad3153ded674786a2846c7690c82c0
Alexander Köhn <kohnalex2@gmail.com>
2024-02-22 11:08:22 +0100

    feat(nvim): tweak neoscroll speed

--- nvim/.config/nvim/lua/user/plugins/neoscroll.lua
+++ nvim/.config/nvim/lua/user/plugins/neoscroll.lua
@@ -10,3 +10,17 @@ require("neoscroll").setup({
   post_hook = nil, -- Function to run after the scrolling animation ends
   performance_mode = false, -- Disable "Performance Mode" on all buffers.
 })
+
+local t = {}
+-- Syntax: t[keys] = {function, {function arguments}}
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } }
+t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "240" } }
+t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "240" } }
+t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
+t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
+t["zt"] = { "zt", { "100" } }
+t["zz"] = { "zz", { "100" } }
+t["zb"] = { "zb", { "100" } }
+
+require("neoscroll.config").set_mappings(t)
